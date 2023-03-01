b767022f9db82dc899ed982defbf61020de33167
Bagrat <bagratavanesian@yandex.ru>
2023-03-01 14:53:00 +0300

    feat: Добавить аннотации полям модели Item.

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -3,15 +3,23 @@ package ru.practicum.shareit.item.model;
 import lombok.Data;
 import ru.practicum.shareit.request.ItemRequest;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
 /**
  * TODO Sprint add-controllers.
  */
 @Data
 public class Item {
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String description;
+    @NotNull
     private Boolean isAvailable;
+    @PositiveOrZero
     private Long owner;
     private ItemRequest request;
 }
