0ff08091d5ae6e8b2362437ae80ae6dd336c4342
Bagrat <bagratavanesian@yandex.ru>
2023-03-01 14:43:41 +0300

    feat: Заполнить модель Item.

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Data;
+import ru.practicum.shareit.request.ItemRequest;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class Item {
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean isAvailable;
+    private Long owner;
+    private ItemRequest request;
 }
