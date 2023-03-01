e9e9f8857dd4c815f452d1d46ea292cd1196b711
Bagrat <bagratavanesian@yandex.ru>
2023-03-01 14:50:08 +0300

    feat: Заполнить модель User.

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.user;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class User {
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String email; //Must be uniq
 }
