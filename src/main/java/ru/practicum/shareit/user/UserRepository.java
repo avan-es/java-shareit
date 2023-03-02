689cca7b502c877afe305db84ad3b28ffd53d7fa
Bagrat <bagratavanesian@yandex.ru>
2023-03-02 13:08:34 +0300

    feat: Рефакторинг

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface UserRepository {
     User addUser (User user);
     User getUserById(Long id);
-    User updateUser(User user);
+    User updateUser(UserDto userDto);
     void deleteUser(Long id);
     List<User> getAllUsers();
 }
