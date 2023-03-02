4a5572ba7e814c62217cfcf8612e197e7c6fb39b
Bagrat <bagratavanesian@yandex.ru>
2023-03-02 09:59:53 +0300

    feat: Добавить эендпоинт addUser.

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,25 @@
 package ru.practicum.shareit.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
 @RequestMapping(path = "/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+@PostMapping
+    public User addUser (@Valid @RequestBody User user){
+    return userService.addUser(user);
+}
 }
