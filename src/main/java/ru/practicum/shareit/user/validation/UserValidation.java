688f8c62dff503c06403ca86bffeded2d89eca93
Bagrat <bagratavanesian@yandex.ru>
2023-03-02 20:18:57 +0300

    fix: Рефакторинг

--- src/main/java/ru/practicum/shareit/user/validation/UserValidation.java
+++ src/main/java/ru/practicum/shareit/user/validation/UserValidation.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.user;
+package ru.practicum.shareit.user.validation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Component;
 import ru.practicum.shareit.exeptions.ModelConflictException;
 import ru.practicum.shareit.exeptions.ModelValidationException;
 import ru.practicum.shareit.exeptions.NotFoundException;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
