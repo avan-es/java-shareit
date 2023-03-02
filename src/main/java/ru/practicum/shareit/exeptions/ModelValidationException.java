de9aae44cb6e150aed6c0a81bfc57194336255c8
Bagrat <bagratavanesian@yandex.ru>
2023-03-02 09:58:56 +0300

    feat: Добавить исключение ModelValidationException.

---
+++ src/main/java/ru/practicum/shareit/exeptions/ModelValidationException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exeptions;
+
+public class ModelValidationException extends RuntimeException{
+    public ModelValidationException (final String message) {
+        super (message);
+    }
+}
