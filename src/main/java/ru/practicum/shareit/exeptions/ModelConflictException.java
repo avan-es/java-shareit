39727d911db214103539d0a39b9f2020d7fbb0f8
Bagrat <bagratavanesian@yandex.ru>
2023-03-02 09:59:11 +0300

    feat: Добавить исключение ModelConflictException.

---
+++ src/main/java/ru/practicum/shareit/exeptions/ModelConflictException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exeptions;
+
+public class ModelConflictException extends RuntimeException{
+    public ModelConflictException(final String message) {
+        super (message);
+    }
+}
