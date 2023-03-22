30e61d779152d2cf0a7933082169b556d77df358
Bagrat <bagratavanesian@yandex.ru>
2023-03-22 22:29:03 +0300

    feat: модифицировать модель под Entity

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,14 +1,20 @@
 package ru.practicum.shareit.user.model;
 
 import lombok.Data;
-
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
+@Entity
+@Table(name = "users")
 @Data
 public class User {
+    @Id
     private Long id;
     @NotBlank
     private String name;
     @NotBlank
     private String email;
+
 }
