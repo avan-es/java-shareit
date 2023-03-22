1afcf120ec40d2176dc10677162aa83c64f85839
Bagrat <bagratavanesian@yandex.ru>
2023-03-22 22:29:31 +0300

    feat: модифицировать модель под Entity

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,37 @@
 package ru.practicum.shareit.booking;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.Data;
+import ru.practicum.shareit.booking.enums.Status;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "bookings")
+@Data
 public class Booking {
+    @Id
+    private Long id;
+
+    @Column(name = "start_time")
+    @NotNull
+    private LocalDate startTime;
+
+    @Column(name = "end_time")
+    @NotNull
+    private LocalDate endTime;
+
+    @Column(name = "item_id")
+    @NotNull
+    private Long itemId;
+
+    @Column(name = "booker_id")
+    @NotNull
+    private Long bookerId;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Status status;
+
 }
