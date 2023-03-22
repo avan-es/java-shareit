97947a29e2fee1f62bd0c9039939696457507ea8
Bagrat <bagratavanesian@yandex.ru>
2023-03-22 22:29:16 +0300

    feat: модифицировать модель под Entity

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,22 +1,36 @@
 package ru.practicum.shareit.item.model;
 
 import lombok.Data;
-import ru.practicum.shareit.request.ItemRequest;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
+@Entity
+@Table(name = "items")
 @Data
 public class Item {
+    @Id
     private Long id;
+
     @NotBlank @NotNull
     private String name;
+
     @NotBlank @NotNull
     private String description;
+
+    @Column(name = "is_available")
     @NotBlank @NotNull
     private Boolean available;
+
+    @Column(name = "owner_id")
     @PositiveOrZero
     private Long owner;
-    private ItemRequest request;
+
+    @Column(name = "request_id")
+    private Long request;
 }
