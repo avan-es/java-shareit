ae850519391fc8117414c984690fe71c38dbacdc
Bagrat <bagratavanesian@yandex.ru>
2023-03-23 23:10:46 +0300

    feat: реализовать BookingDto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.booking.dto;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import ru.practicum.shareit.booking.enums.Status;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@RequiredArgsConstructor
 public class BookingDto {
+    private Long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private User booker;
+    private Status status;
+    private Item item;
 }
