e5064f30705e03eea49f58dca1affae32a434a7c
Bagrat <bagratavanesian@yandex.ru>
2023-03-23 23:09:02 +0300

    feat: добавить интерфейс сервиса бронирования

---
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -0,0 +1,9 @@
+package ru.practicum.shareit.booking.service;
+
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+public interface BookingService {
+    BookingDto saveBooking(Booking booking, Long userId);
+}
