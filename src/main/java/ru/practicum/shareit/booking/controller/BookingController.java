544f762e6532ee186de4450c455d13cea40a4137
Bagrat <bagratavanesian@yandex.ru>
2023-03-23 23:11:09 +0300

    feat: реализовать метод addBooking в BookingController

---
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -0,0 +1,27 @@
+package ru.practicum.shareit.booking.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.service.BookingService;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+/**
+ * TODO Sprint add-bookings.
+ */
+@RestController
+@RequestMapping(path = "/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    @PostMapping
+    public BookingDto addBooking(@RequestBody Booking booking,
+                                 @RequestHeader(value = "X-Sharer-User-Id") Long userId) {
+        return bookingService.saveBooking(booking, userId);
+    }
+
+}
