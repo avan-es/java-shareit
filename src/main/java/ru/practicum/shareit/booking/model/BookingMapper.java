0fff824a77b568abfc5d37606b101bd95aea6e9e
Bagrat <bagratavanesian@yandex.ru>
2023-03-24 19:08:09 +0300

    feat: добавить BookingMapper

--- src/main/java/ru/practicum/shareit/booking/model/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/model/BookingMapper.java
@@ -1,14 +1,14 @@
 package ru.practicum.shareit.booking.model;
 
 import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.item.dto.ItemBookingDto;
+import ru.practicum.shareit.user.dto.UserBookingDto;
 
 
 public enum BookingMapper {
     INSTANT;
 
-    public BookingDto toBookingDto(Booking booking, Item item, User user) {
+    public BookingDto toBookingDto(Booking booking, ItemBookingDto item, UserBookingDto user) {
         BookingDto bookingDto = new BookingDto();
         bookingDto.setId(booking.getId());
         bookingDto.setStart(booking.getStart());
