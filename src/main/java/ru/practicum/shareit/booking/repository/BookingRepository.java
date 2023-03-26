5344f7c6979bd1ab292d85292315042617dc9283
Bagrat <bagratavanesian@yandex.ru>
2023-03-26 20:12:53 +0300

    fix: рефакторинг

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -12,7 +12,6 @@ import java.util.List;
 @Component("dbBookingRepository")
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-//Booker
     @Query("select new ru.practicum.shareit.booking.dto.BookingDto(bk.id, bk.start, bk.end, bk.status, booker.id, it.id, it.name) " +
             "from Booking as bk " +
             "join Item as it on bk.itemId = it.id " +
@@ -58,8 +57,6 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
             "AND bk.status = ?2 " +
             "order by bk.start DESC ")
     List<BookingDto> userFindAllWaitingOrRejected(Long userId, String status);
-
-    //Owner
 
     @Query("select new ru.practicum.shareit.booking.dto.BookingDto(bk.id, bk.start, bk.end, bk.status, booker.id, it.id, it.name) " +
             "from Booking as bk " +
@@ -107,48 +104,19 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
             "order by bk.start DESC ")
     List<BookingDto> ownerFindAllWaitingOrRejected(Long userId, String status);
 
-
-    List<BookingDto> getBookingByItemIdAndEndIsBefore(Long itemId, LocalDateTime now);
-/*    @Query(value = "select * " +
-            "FROM BOOKINGS b " +
-            "WHERE b.ITEM_ID = ?1 " +
-            "AND b.START_DATE < ?2 " +
-            "ORDER BY b.end_date  DESC LIMIT 1 ", nativeQuery = true)*/
-
-/*    @Query(value = "select b.id AS bookingId, b.booker_id AS bookerId, b.start_date AS bookingDate " +
-            "FROM BOOKINGS b " +
-            "WHERE b.ITEM_ID = ?1 " +
-            "AND b.START_DATE < ?2 " +
-            "ORDER BY b.START_DATE  DESC LIMIT 1 ", nativeQuery = true)*/
-  /*  @Query(value = "select bk.ID AS id, bk.START_DATE as start, bk.END_DATE AS end, bk.STATUS AS status, booker.ID, it.ID, it.NAME " +
-            "from BOOKINGS as bk " +
-            "join Items as it on bk.ITEM_ID  = it.ID " +
-            "join Users as booker on bk.BOOKER_ID  = booker.id " +
-            "WHERE bk.ITEM_ID = ?1 " +
-            "AND bk.START_DATE < ?2 " +
-            "order by start  DESC  LIMIT 1", nativeQuery = true)*/
-@Query(value = "select * " +
+    @Query(value = "select * " +
         "FROM BOOKINGS b " +
         "WHERE b.ITEM_ID = ?1 " +
         "AND b.START_DATE < ?2 " +
         "ORDER BY b.end_date  DESC LIMIT 1 ", nativeQuery = true)
     Booking getLastBooking (Long itemId, LocalDateTime now);
 
-    /*@Query(value = "select * " +
-            "FROM BOOKINGS b " +
-            "WHERE b.ITEM_ID = ?1 " +
-            "AND b.START_DATE > ?2 " +
-            "ORDER BY b.end_date  DESC LIMIT 1 ", nativeQuery = true)*/
-    /*@Query(value = "select b.id AS bookingId, b.booker_id AS bookerId, b.start_date AS bookingDate " +
-            "FROM BOOKINGS b " +
-            "WHERE b.ITEM_ID = ?1 " +
-            "AND b.START_DATE > ?2 " +
-            "ORDER BY b.START_DATE  DESC LIMIT 1 ", nativeQuery = true)*/
     @Query(value = "select * " +
             "FROM BOOKINGS b " +
             "WHERE b.ITEM_ID = ?1 " +
             "AND b.START_DATE > ?2 " +
-            "ORDER BY b.end_date  DESC LIMIT 1 ", nativeQuery = true)
+            "AND NOT b.status = 'REJECTED' " +
+            "ORDER BY b.end_date  ASC LIMIT 1 ", nativeQuery = true)
     Booking getNextBooking (Long itemId, LocalDateTime now);
 
 
