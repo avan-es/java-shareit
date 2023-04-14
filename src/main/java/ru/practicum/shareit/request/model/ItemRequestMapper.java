26bcd2516048ff9396e937f670e8f0e565965881
Bagrat <bagratavanesian@yandex.ru>
2023-04-15 01:21:45 +0300

    fix: убрать лишние методы

--- src/main/java/ru/practicum/shareit/request/model/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequestMapper.java
@@ -1,10 +1,7 @@
 package ru.practicum.shareit.request.model;
 
-import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
-import ru.practicum.shareit.request.dto.ItemResponseForRequestDto;
 
-import java.util.List;
 
 public enum ItemRequestMapper {
     INSTANT;
@@ -18,31 +15,4 @@ public enum ItemRequestMapper {
         return itemRequestDto;
     }
 
-    public ItemRequest toItemRequest(ItemRequestDto itemRequestDto) {
-        ItemRequest itemRequest = new ItemRequest();
-        itemRequest.setId(itemRequestDto.getId());
-        itemRequest.setDescription(itemRequestDto.getDescription());
-        itemRequest.setRequesterId(itemRequestDto.getRequestorId());
-        itemRequest.setCreated(itemRequestDto.getCreated());
-        return itemRequest;
-    }
-
-    public ItemResponseForRequestDto toResponseWithItems(ItemRequest request, List<ItemDto> responses) {
-        ItemResponseForRequestDto response = new ItemResponseForRequestDto();
-        response.setId(request.getId());
-        response.setRequestId(request.getId());
-        response.setDescription(request.getDescription());
-        response.setCreated(request.getCreated());
-        response.setItems(responses);
-        return response;
-    }
-
-    public ItemResponseForRequestDto toResponseWithoutItems(ItemRequest request) {
-        ItemResponseForRequestDto response = new ItemResponseForRequestDto();
-        response.setId(request.getId());
-        response.setRequestId(request.getId());
-        response.setDescription(request.getDescription());
-        response.setCreated(request.getCreated());
-        return response;
-    }
 }
