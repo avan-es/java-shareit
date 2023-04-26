791ca16e69e39c45b916ba0166f09498ab36ae4d
Bagrat <bagratavanesian@yandex.ru>
2023-04-26 16:34:45 +0300

    feat: доработать классы

--- shareIt-gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
+++ shareIt-gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
@@ -3,10 +3,14 @@ package ru.practicum.shareit.request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.client.BaseClient;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+import java.util.Map;
 
 @Service
 public class ItemRequestClient extends BaseClient {
@@ -21,5 +25,29 @@ public class ItemRequestClient extends BaseClient {
                         .requestFactory(HttpComponentsClientHttpRequestFactory::new)
                         .build()
         );
+    }
+
+    public ResponseEntity<Object> add(Long userId, ItemRequestDto itemRequestDto) {
+        return post("", userId, itemRequestDto);
+    }
+
+    public ResponseEntity<Object> getUsersRequests(Long userId) {
+        return get("", userId);
+    }
+
+    public ResponseEntity<Object> getRequests(Integer from, Integer size, Long userId) {
+        if (from == null) {
+            return get("/all", userId);
+        } else {
+            Map<String, Object> parameters = Map.of(
+                    "from", from,
+                    "size", size
+            );
+            return get("/all?from={from}&size={size}", userId, parameters);
+        }
+    }
+
+    public ResponseEntity<Object> getRequestById(Long userId, Long requestId) {
+        return get("/" + requestId, userId);
     }
 }
--- shareIt-gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ shareIt-gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -2,17 +2,48 @@ package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
 
-@Controller
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+
+//@Controller
+@RestController
 @RequestMapping(path = "/requests")
 @RequiredArgsConstructor
 @Slf4j
 @Validated
 public class ItemRequestController {
 
-    private final ItemRequestClient itemRequestClient;
+    private final ItemRequestClient requestClient;
 
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                      @RequestBody @Valid ItemRequestDto request) {
+        return requestClient.add(userId, request);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getUsersRequests(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        return requestClient.getUsersRequests(userId);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Object> getRequests(@PositiveOrZero
+                                              @RequestParam(value = "from", required = false) Integer from,
+                                              @Positive @RequestParam(value = "size", required = false) Integer size,
+                                              @RequestHeader("X-Sharer-User-Id") Long userId) {
+        return requestClient.getRequests(from, size, userId);
+    }
+
+    @GetMapping("/{requestId}")
+    public ResponseEntity<Object> getRequestById(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                 @PathVariable Long requestId) {
+        return requestClient.getRequestById(userId, requestId);
+    }
 }
